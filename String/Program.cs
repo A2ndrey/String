7bb6bfde8debbd36337d8e438b41c6b5b757689a
AnDreW <andrew21ls@yandex.ru>
2020-03-02 20:53:24 +0300

    initial commit

--- String/Program.cs
+++ String/Program.cs
@@ -10,11 +10,48 @@ namespace String
     {
         static void Main(string[] args)
         {
-            char symb = Convert.ToChar(Console.ReadLine());
-
-            Console.WriteLine(symb);
+            // char
+            // IsCharDigit();
+            // part_of_alphabet();
+            //part_of_alphabet(false);
+            change_char_case();
 
             Console.ReadKey();
+        }
+
+        private static void change_char_case()
+        {
+            const int code = 32;
+
+            char c = Convert.ToChar(Console.ReadLine());
+            if (((char)c >= 97) && ((char)c <= 122))
+            {
+                Console.WriteLine((char)(c - code));
+            }
+            else
+            { Console.WriteLine((char)(c + code)); }
+        }
+
+        private static void part_of_alphabet(bool fromstart = true)
+        {
+            char c = Convert.ToChar(Console.ReadLine());
+            char startchar = fromstart ? 'a' : c;
+            char endchar = fromstart ? c : 'z';
+
+            for (char a = startchar; a <= endchar; a++)
+            {
+                Console.Write(a + " ");
+            }
+
+        }
+
+        private static void IsCharDigit()
+        {
+            int c = Convert.ToChar(Console.ReadLine());
+
+            if (c >= '0' && c <= '9') { Console.WriteLine("YES"); }
+            else { Console.WriteLine("NO"); }
+
         }
     }
 }
